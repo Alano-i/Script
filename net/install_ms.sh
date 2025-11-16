5038c21ca3b4bba22805fdc0079a42eaf7ff6222
Alano-i <68833595+Alano-i@users.noreply.github.com>
2025-11-16 13:37:54 +0800

    Create install_ms.sh

---
+++ net/install_ms.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -e
+
+echo
+echo "███╗   ███╗██╗██╗  ██╗ ██████╗ ███╗   ███╗ ██████╗     ███████╗███╗   ███╗██████╗  █████╗ ████████╗██████╗ ███╗   ██╗███████╗
+████╗ ████║██║██║  ██║██╔═══██╗████╗ ████║██╔═══██╗    ██╔════╝████╗ ████║██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗████╗  ██║██╔════╝
+██╔████╔██║██║███████║██║   ██║██╔████╔██║██║   ██║    ███████╗██╔████╔██║██████╔╝███████║   ██║   ██║  ██║██╔██╗ ██║███████╗
+██║╚██╔╝██║██║██╔══██║██║   ██║██║╚██╔╝██║██║   ██║    ╚════██║██║╚██╔╝██║██╔══██╗██╔══██║   ██║   ██║  ██║██║╚██╗██║╚════██║
+██║ ╚═╝ ██║██║██║  ██║╚██████╔╝██║ ╚═╝ ██║╚██████╔╝    ███████║██║ ╚═╝ ██║██║  ██║██║  ██║   ██║   ██████╔╝██║ ╚████║███████║
+╚═╝     ╚═╝╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝ ╚═════╝     ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+                                                                                                                             "
+
+echo "============================="
+echo "  请选择要安装的组件"
+echo "============================="
+echo "1) 安装 Mihomo"
+echo "2) 安装 SmartDNS"
+echo "0) 退出脚本"
+echo "============================="
+read -p "请输入选项 (0, 1 或 2): " choice
+
+case "$choice" in
+	1)
+		echo "即将安装 Mihomo"
+		curl -sS -O https://raw.githubusercontent.com/Alano-i/Script/refs/heads/main/net/install_mihomo.sh || { echo "下载 install_mihomo.sh 失败"; exit 1; }
+		chmod +x install_mihomo.sh
+		./install_mihomo.sh
+		;;
+	2)
+		echo "即将安装 SmartDNS"
+		curl -sS -O https://raw.githubusercontent.com/Alano-i/Script/refs/heads/main/net/install_smartdns.sh || { echo "下载 install_smartdns.sh 失败"; exit 1; }
+		chmod +x install_smartdns.sh
+		./install_smartdns.sh
+		;;
+		0)
+			echo "已退出安装"
+			exit 0
+			;;
+		*)
+			echo "ERROR: 无效的选项，请输入 0、1 或 2"
+			exit 1
+			;;
+esac
+
+exit 0
